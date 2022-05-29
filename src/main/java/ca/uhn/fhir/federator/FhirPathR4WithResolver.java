c9a401424ca257f6e77d8578e61345023ecad581
bartdecu <decuypeb@gmail.com>
2022-05-29 21:29:30 +0200

    added _include functionality

---
+++ src/main/java/ca/uhn/fhir/federator/FhirPathR4WithResolver.java
@@ -0,0 +1,58 @@
+package ca.uhn.fhir.federator;
+
+
+import java.util.List;
+import java.util.Optional;
+
+import org.hl7.fhir.exceptions.FHIRException;
+import org.hl7.fhir.instance.model.api.IBase;
+import org.hl7.fhir.r4.hapi.ctx.HapiWorkerContext;
+import org.hl7.fhir.r4.model.Base;
+import org.hl7.fhir.r4.utils.FHIRPathEngine;
+
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.context.support.IValidationSupport;
+import ca.uhn.fhir.fhirpath.FhirPathExecutionException;
+import ca.uhn.fhir.fhirpath.IFhirPath;
+
+public class FhirPathR4WithResolver implements IFhirPath {
+
+  private FHIRPathEngine myEngine;
+
+  public FhirPathR4WithResolver(FhirContext theCtx, ClientRegistry cr, ResourceRegistry rr) {   
+    IValidationSupport validationSupport = theCtx.getValidationSupport();
+    myEngine = new FHIRPathEngine(new HapiWorkerContext(theCtx, validationSupport));
+    myEngine.setHostServices(new EvaluationContextWithResolver(myEngine.getHostServices(),theCtx, cr, rr));
+  }
+
+  @SuppressWarnings("unchecked")
+  @Override
+  public <T extends IBase> List<T> evaluate(IBase theInput, String thePath, Class<T> theReturnType) {
+    List<Base> result;
+    try {
+      result = myEngine.evaluate((Base) theInput, thePath);
+    } catch (FHIRException e) {
+      throw new FhirPathExecutionException(e);
+    }
+
+    for (Base next : result) {
+      if (!theReturnType.isAssignableFrom(next.getClass())) {
+        throw new FhirPathExecutionException("FluentPath expression \"" + thePath + "\" returned unexpected type " + next.getClass().getSimpleName() + " - Expected " + theReturnType.getName());
+      }
+    }
+
+    return (List<T>) result;
+  }
+
+  @Override
+  public <T extends IBase> Optional<T> evaluateFirst(IBase theInput, String thePath, Class<T> theReturnType) {
+    return evaluate(theInput, thePath, theReturnType).stream().findFirst();
+  }
+
+  @Override
+  public void parse(String theExpression) {
+    myEngine.parse(theExpression);
+  }
+
+
+}
--- src/main/java/ca/uhn/fhir/federator/ParameterExecutor.java
+++ src/main/java/ca/uhn/fhir/federator/ParameterExecutor.java
@@ -15,8 +15,8 @@ import org.apache.commons.lang3.StringUtils;
 import org.hl7.fhir.instance.model.api.IBase;
 import org.hl7.fhir.instance.model.api.IBaseResource;
 import org.hl7.fhir.r4.model.Bundle;
-import org.hl7.fhir.r4.model.Identifier;
 import org.hl7.fhir.r4.model.Bundle.BundleLinkComponent;
+import org.hl7.fhir.r4.model.Identifier;
 
 import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.fhirpath.IFhirPath;
@@ -31,11 +31,12 @@ public class ParameterExecutor {
     private SearchParam2FhirPathRegistry s2f;
     private Map<String, List<IBaseResource>> resourceCachePerParameter;
 
-    public ParameterExecutor(List<ParsedUrl> urlsPerParameter, ResourceRegistry rr, ClientRegistry cr, FhirContext ctx, SearchParam2FhirPathRegistry s2f) {
+    public ParameterExecutor(List<ParsedUrl> urlsPerParameter, ResourceRegistry rr, ClientRegistry cr, FhirContext ctx,
+            SearchParam2FhirPathRegistry s2f) {
         this.urlsPerParameter = urlsPerParameter;
         this.rr = rr;
         this.cr = cr;
-        this.ctx =ctx;
+        this.ctx = ctx;
         this.s2f = s2f;
         resourceCachePerParameter = new HashMap<>();
     }
@@ -45,7 +46,7 @@ public class ParameterExecutor {
 
             ParsedUrl url = urlsPerParameter.get(i);
             String resource = url.getResource();
-            List<ParsedUrl> executableUrls = createExecutableUrl(resourceCachePerParameter, url, rr, this.ctx, this.s2f);
+            List<ParsedUrl> executableUrls = createExecutableUrl(url);
             for (ParsedUrl executableUrl : executableUrls) {
                 List<IBaseResource> result = null;
                 if (!executableUrl.isExecutable()) {
@@ -77,61 +78,52 @@ public class ParameterExecutor {
 
     }
 
-    public static List<ParsedUrl> createExecutableUrl(Map<String, List<IBaseResource>> idMap, ParsedUrl url,
-            ResourceRegistry rr, FhirContext ctx, SearchParam2FhirPathRegistry s2f) {
+    public List<ParsedUrl> createExecutableUrl(ParsedUrl url) {
         List<ParsedUrl> retVal = new ArrayList<>();
-        String resource = url.getResource();
-        if (url.getPlaceholder() != null) {
-            if (idMap.get(url.getPlaceholder().getKey()) != null) {
-                List<IBaseResource> list = idMap.get(url.getPlaceholder().getKey());
-                List<String> identifiers = getIdentifiersFromResources(list, url.getPlaceholder(), ctx,
-                        s2f);
-                int batch = rr.getMaxOr4Resource(resource);
-                int counter = 0;
-                List<String> idBatch = new ArrayList<>();
-                for (String identifier : identifiers) {
-                    if (counter < batch) {
-                        idBatch.add(identifier);
-                    } else {
-                        counter = 0;
-                        retVal.add(new ParsedUrl(resource, url.getKey(), StringUtils.join(idBatch, ",")));
-                        idBatch.clear();
-                        idBatch.add(identifier);
+        List<String> resources = Arrays.asList(url.getResource());
+        if (url.getResource() == null) {
+            // _include case
+            List<IBaseResource> list = resourceCachePerParameter.get(url.getPlaceholder().getKey());
+            DefaultMapEntry<String, List<String>> placeholder = url.getPlaceholder();
+            DefaultMapEntry<String, List<String>> mp = /*placeholder;*/new DefaultMapEntry<String, List<String>>(placeholder.getKey(),
+                    placeholder.getValue().stream().filter(x -> !"identifier".equals(x)).collect(Collectors.toList()));
+            resources = getResources(list, mp).stream().map(x -> x.getClass().getSimpleName()).distinct()
+                    .collect(Collectors.toList());
+
+        }
+        for (String resource : resources) {
+            if (url.getPlaceholder() != null) {
+                if (resourceCachePerParameter.get(url.getPlaceholder().getKey()) != null) {
+                    List<IBaseResource> list = resourceCachePerParameter.get(url.getPlaceholder().getKey());
+                    List<String> identifiers = getIdentifiersFromResources(list, url.getPlaceholder());
+                    int batch = rr.getMaxOr4Resource(resource);
+                    int counter = 0;
+                    List<String> idBatch = new ArrayList<>();
+                    for (String identifier : identifiers) {
+                        if (counter < batch) {
+                            idBatch.add(identifier);
+                        } else {
+                            counter = 0;
+                            retVal.add(new ParsedUrl(resource, url.getKey(), StringUtils.join(idBatch, ",")));
+                            idBatch.clear();
+                            idBatch.add(identifier);
+                        }
+                        counter++;
                     }
-                    counter++;
+                    if (!idBatch.isEmpty()) {
+                        retVal.add(new ParsedUrl(resource, url.getKey(), StringUtils.join(idBatch, ",")));
+                    }
                 }
-                if (!idBatch.isEmpty()) {
-                    retVal.add(new ParsedUrl(resource, url.getKey(), StringUtils.join(idBatch, ",")));
-                }
+            } else {
+                retVal.add(url);
             }
-        } else {
-            retVal.add(url);
         }
         return retVal;
     }
 
-    private static List<String> getIdentifiersFromResources(List<IBaseResource> list,
-            DefaultMapEntry<String, List<String>> placeholder, FhirContext ctx, SearchParam2FhirPathRegistry s2f) {
-        final List<IBase> toProcess = new ArrayList<>(list);
-        final List<IBase> nextRound = new ArrayList<>();
-
-        placeholder.getValue().forEach(searchParam -> {
-
-            toProcess.forEach(inputResource -> {
-                String resourceName = inputResource.getClass().getSimpleName();
-                List<IBase> outputs;
-                IFhirPath fhirPath = ctx.newFhirPath();
-                String fp = s2f.getFhirPath(resourceName, searchParam);
-                outputs = fhirPath.evaluate(inputResource, fp, IBase.class);
-                nextRound.addAll(outputs);
-
-            });
-
-            toProcess.clear();
-            toProcess.addAll(nextRound);
-            nextRound.clear();
-
-        });
+    private List<String> getIdentifiersFromResources(List<IBaseResource> list,
+            DefaultMapEntry<String, List<String>> placeholder) {
+        final List<IBase> toProcess = getResources(list, placeholder);
 
         List<String> identifiers = toProcess.stream()
                 .flatMap(x -> {
@@ -161,25 +153,48 @@ public class ParameterExecutor {
         return identifiers;
     }
 
+    private List<IBase> getResources(List<IBaseResource> list, DefaultMapEntry<String, List<String>> placeholder) {
+        final List<IBase> toProcess = new ArrayList<>(list);
+        final List<IBase> nextRound = new ArrayList<>();
+
+        placeholder.getValue().forEach(searchParam -> {
+
+            toProcess.forEach(inputResource -> {
+                String resourceName = inputResource.getClass().getSimpleName();
+                List<IBase> outputs;
+                //IFhirPath fhirPath = ctx.newFhirPath();
+                IFhirPath fhirPath = new FhirPathR4WithResolver(ctx,cr,rr);
+                String fp = s2f.getFhirPath(resourceName, searchParam);
+                outputs = fhirPath.evaluate(inputResource, fp, IBase.class);
+                nextRound.addAll(outputs);
+            });
+
+            toProcess.clear();
+            toProcess.addAll(nextRound);
+            nextRound.clear();
+
+        });
+        return toProcess;
+    }
 
     private static List<IBaseResource> getResultsForURL(IGenericClient client, String completeUrl) {
         Bundle bundle = client.search().byUrl(completeUrl).returnBundle(Bundle.class).execute();
         List<BundleLinkComponent> next = bundle.getLink().stream().filter(link -> link.getRelation().equals("next"))
-            .collect(Collectors.toList());
-    
+                .collect(Collectors.toList());
+
         List<IBaseResource> own = bundle.getEntry().stream().filter(bec -> {
-          return bec.getResource() instanceof IBaseResource;
+            return bec.getResource() instanceof IBaseResource;
         })
-            .map(bec -> ((IBaseResource) bec.getResource())).collect(Collectors.toList());
+                .map(bec -> ((IBaseResource) bec.getResource())).collect(Collectors.toList());
         ourLog.info("Client request Url: {} #{}", completeUrl, own.size());
         List<IBaseResource> out = new ArrayList<>();
         out.addAll(own);
         if (!next.isEmpty()) {
-          List<IBaseResource> other = getResultsForURL(client, next.get(0).getUrl());
-          out.addAll(other);
+            List<IBaseResource> other = getResultsForURL(client, next.get(0).getUrl());
+            out.addAll(other);
         }
         return out;
-      }
+    }
 
     public void setCachedResources(Map<String, List<IBaseResource>> resourceCachePerParameter) {
         this.resourceCachePerParameter = resourceCachePerParameter;
